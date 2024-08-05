594ed1700b90fc0e0c66d065b94ea1da851254aa
Vinícius Chab <chabvini@gmail.com>
2024-08-05 15:28:49 -0300

    Updated angular version to 18

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { DynamicContentService } from 'src/app/services/dynamic-content/dynamic-content.service';
 import { DynamicPage } from '../../models/dynamic-page.model';
 
@@ -7,7 +7,11 @@ import { DynamicPage } from '../../models/dynamic-page.model';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule],
   providers: [DynamicContentService],
 })
 export class HomeComponent implements OnInit {
